09155febea603d8eb62ff90db44d612e54ac3f97
Mark <60153141+markmacwan@users.noreply.github.com>
2020-06-08 19:47:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -18,36 +18,32 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-#	df= pd.read_csv("spam.csv", encoding="latin-1")
-#	df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
-#	# Features and Labels
-#	df['label'] = df['class'].map({'ham': 0, 'spam': 1})
-#	X = df['message']
-#	y = df['label']
-#	
-#	# Extract Feature With CountVectorizer
-#	cv = CountVectorizer()
-#	X = cv.fit_transform(X) # Fit the Data
-#    
-#    pickle.dump(cv, open('tranform.pkl', 'wb'))
-#    
-#    
-#	from sklearn.model_selection import train_test_split
-#	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-#	#Naive Bayes Classifier
-#	from sklearn.naive_bayes import MultinomialNB
-#
-#	clf = MultinomialNB()
-#	clf.fit(X_train,y_train)
-#	clf.score(X_test,y_test)
-#    filename = 'nlp_model.pkl'
-#    pickle.dump(clf, open(filename, 'wb'))
+	df= pd.read_csv("spam.csv", encoding="latin-1")
+	df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
+	# Features and Labels
+	df['label'] = df['class'].map({'ham': 0, 'spam': 1})
+	X = df['message']
+	y = df['label']
+	
+	# Extract Feature With CountVectorizer
+	cv = CountVectorizer()
+	X = cv.fit_transform(X) # Fit the Data
     
-	#Alternative Usage of Saved Model
-	# joblib.dump(clf, 'NB_spam_model.pkl')
-	# NB_spam_model = open('NB_spam_model.pkl','rb')
-	# clf = joblib.load(NB_spam_model)
+    pickle.dump(cv, open('tranform.pkl', 'wb'))
+    
+    
+	from sklearn.model_selection import train_test_split
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+	#Naive Bayes Classifier
+	from sklearn.naive_bayes import MultinomialNB
 
+	clf = MultinomialNB()
+	clf.fit(X_train,y_train)
+	clf.score(X_test,y_test)
+    filename = 'nlp_model.pkl'
+    pickle.dump(clf, open(filename, 'wb'))
+   
+	
 	if request.method == 'POST':
 		message = request.form['message']
 		data = [message]
